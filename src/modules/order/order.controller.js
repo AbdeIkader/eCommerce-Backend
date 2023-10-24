314a8d6d49ad2847cc0f80af66de4f32c13784ec
Abdelkader <abdelrahmanabdelkader2002@gmail.com>
2023-10-24 15:25:17 +0300

    Create order online

--- src/modules/order/order.controller.js
+++ src/modules/order/order.controller.js
@@ -4,9 +4,10 @@ import { cartModel } from "../../../Database/models/cart.model.js";
 import { productModel } from "../../../Database/models/product.model.js";
 import { orderModel } from "../../../Database/models/order.model.js";
 
-import Stripe from 'stripe';
-const stripe = new Stripe('sk_test_51NyJGHKigR3f9MS7eTSPzLdqcXn5m52JE6JHdMNDvA6eXyhCoJwhxfObrkgyano5EepvvxkvH1oIn64lxEcAULtI009cJBLA7z');
-
+import Stripe from "stripe";
+const stripe = new Stripe(
+  "sk_test_51NV8e0HVbfRYk4SfG3Ul84cabreiXkPbW1xMugwqvU9is2Z2ICEafTtG6NHLIUdFVIjkiRHYmAPKxCLsCpoU2NnN00LVpHcixz"
+);
 
 const createCashOrder = catchAsyncError(async (req, res, next) => {
   let cart = await cartModel.findById(req.params.id);
@@ -16,7 +17,7 @@ const createCashOrder = catchAsyncError(async (req, res, next) => {
     ? cart.totalPriceAfterDiscount
     : cart.totalPrice;
 
-    console.log(cart.cartItem);
+  console.log(cart.cartItem);
   const order = new orderModel({
     userId: req.user._id,
     cartItem: cart.cartItem,
@@ -28,7 +29,7 @@ const createCashOrder = catchAsyncError(async (req, res, next) => {
 
   // console.log(order);
   if (order) {
-    let options = cart.cartItem.map(item=> ({
+    let options = cart.cartItem.map((item) => ({
       updateOne: {
         filter: { _id: item.productId },
         update: { $inc: { quantity: -item.quantity, sold: item.quantity } },
@@ -39,86 +40,133 @@ const createCashOrder = catchAsyncError(async (req, res, next) => {
 
     await cartModel.findByIdAndDelete(req.params.id);
 
-    return res.status(201).json({message:"success",order})
-  }else{
-    next(new AppError("Error in cart ID",404))
+    return res.status(201).json({ message: "success", order });
+  } else {
+    next(new AppError("Error in cart ID", 404));
   }
-
-  
 });
 
 const getSpecificOrder = catchAsyncError(async (req, res, next) => {
-
   console.log(req.user._id);
 
-  let order = await orderModel.findOne({ userId: req.user._id }).populate('cartItems.productId')
+  let order = await orderModel
+    .findOne({ userId: req.user._id })
+    .populate("cartItems.productId");
 
-  res.status(200).json({message:"success",order})
-
+  res.status(200).json({ message: "success", order });
 });
 
 const getAllOrders = catchAsyncError(async (req, res, next) => {
+  let orders = await orderModel.findOne({}).populate("cartItems.productId");
 
-  let orders = await orderModel.findOne({}).populate('cartItems.productId')
-
-  res.status(200).json({message:"success",orders})
-
+  res.status(200).json({ message: "success", orders });
 });
 
 const createCheckOutSession = catchAsyncError(async (req, res, next) => {
-
   let cart = await cartModel.findById(req.params.id);
+  if(!cart) return next(new AppError("Cart was not found",404))
+
+  console.log(cart);
 
   // console.log(cart);
   let totalOrderPrice = cart.totalPriceAfterDiscount
     ? cart.totalPriceAfterDiscount
     : cart.totalPrice;
 
-
- let sessions = await stripe.checkout.sessions.create({
-    line_items:[
+  let sessions = await stripe.checkout.sessions.create({
+    line_items: [
       {
-        price_data:{
-          currency:'egp',
+        price_data: {
+          currency: "egp",
           unit_amount: totalOrderPrice * 100,
-          product_data:{
-            name:req.user.name,
-          }
+          product_data: {
+            name: req.user.name,
+          },
         },
-        quantity:1
-      }
+        quantity: 1,
+      },
     ],
-    mode:'payment',
-    success_url:'https://github.com/AbdeIkader',
-    cancel_url:'https://www.linkedin.com/in/abdelrahman-abdelkader-259781215/',
-    customer_email:req.user.email,
-    client_reference_id:req.params.id,
-    metadata:req.body.shippingAddress
- })
+    mode: "payment",
+    success_url: "https://github.com/AbdeIkader",
+    cancel_url: "https://www.linkedin.com/in/abdelrahman-abdelkader-259781215/",
+    customer_email: req.user.email,
+    client_reference_id: req.params.id,
+    metadata: req.body.shippingAddress,
+  });
 
- res.json({message:"success",sessions})
+  res.json({ message: "success", sessions });
 });
 
 const createOnlineOrder = catchAsyncError(async (request, response) => {
-  const sig = request.headers['stripe-signature'].toString()
+  const sig = request.headers["stripe-signature"].toString();
 
   let event;
 
   try {
-    event = stripe.webhooks.constructEvent(request.body, sig, 'whsec_fcatGuOKvXYUQoz5NWSwH9vaqdWXIWsI');
+    event = stripe.webhooks.constructEvent(
+      request.body,
+      sig,
+      "whsec_fcatGuOKvXYUQoz5NWSwH9vaqdWXIWsI"
+    );
   } catch (err) {
     return response.status(400).send(`Webhook Error: ${err.message}`);
-    
   }
 
   // Handle the event
-  if(event.type == "checkout.session.completed"){
-    const checkoutSessionCompleted = event.data.object;
-    console.log("Create order here....",checkoutSessionCompleted);
-  }else{
+  if (event.type == "checkout.session.completed") {
+    // const checkoutSessionCompleted = event.data.object;
+    card(event.data.object,response)
+
+
+  } else {
     console.log(`Unhandled event type ${event.type}`);
   }
-
 });
 
-export {createCashOrder,getSpecificOrder,getAllOrders,createCheckOutSession,createOnlineOrder};
+//https://ecommerce-backend-codv.onrender.com/api/v1/orders/checkOut/6536c48750fab46f309bb950
+
+
+async function card (e,res){
+  let cart = await cartModel.findById(e.client_reference_id);
+
+  if(!cart) return next(new AppError("Cart was not found",404))
+
+  let user = await userModel.findOne({email:e.customer_email})
+  const order = new orderModel({
+    userId: user._id,
+    cartItem: cart.cartItem,
+    totalOrderPrice : e.amount_total/100,
+    shippingAddress: e.metadata.shippingAddress,
+    paymentMethod:"card",
+    isPaid:true,
+    paidAt:Date.now()
+  });
+
+  await order.save();
+
+  // console.log(order);
+  if (order) {
+    let options = cart.cartItem.map((item) => ({
+      updateOne: {
+        filter: { _id: item.productId },
+        update: { $inc: { quantity: -item.quantity, sold: item.quantity } },
+      },
+    }));
+
+    await productModel.bulkWrite(options);
+
+    await cartModel.findByIdAndDelete({userId: user._id});
+
+    return res.status(201).json({ message: "success", order });
+  } else {
+    next(new AppError("Error in cart ID", 404));
+  }
+}
+
+export {
+  createCashOrder,
+  getSpecificOrder,
+  getAllOrders,
+  createCheckOutSession,
+  createOnlineOrder,
+};
